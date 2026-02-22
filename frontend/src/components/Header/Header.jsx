3242678b245af272ffd19ff9aa94ceff56bae252
lakshayagrawal2116-beep <lakshayagrawal2116@gmail.com>
2026-02-22 21:13:54 +0530

    22/02/26 9:13pm

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,8 @@ const Header = () => {
         <div className='header-contents'>
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingerdients and culinary expertise. Our mission is to satisfy your cravings and elevate your dinning expertise,one delicios meal at a time</p>
-            <button>view menu</button>
+
+            <button onClick={() => window.location.href = '#explore-menu'}>view menu</button>
         </div>
     </div>
   )
