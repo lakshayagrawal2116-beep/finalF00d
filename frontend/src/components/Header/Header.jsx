3aad42722e3c4a484a1b9d6b701fa3dd3a52123c
lakshayagrawal2116-beep <lakshayagrawal2116@gmail.com>
2026-02-23 19:54:23 +0530

    23/02/26 7:54pm

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,12 +3,12 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header'>
-        <div className='header-contents'>
-            <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingerdients and culinary expertise. Our mission is to satisfy your cravings and elevate your dinning expertise,one delicios meal at a time</p>
+      <div className='header-contents'>
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingerdients and culinary expertise. Our mission is to satisfy your cravings and elevate your dinning expertise,one delicios meal at a time</p>
 
-            <button onClick={() => window.location.href = '#explore-menu'}>view menu</button>
-        </div>
+        <button onClick={() => window.location.href = '#explore-menu'}>View Menu</button>
+      </div>
     </div>
   )
 }
